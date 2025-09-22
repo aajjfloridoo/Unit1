88f9a1848a7d292368ddb399a126dc9454597fdb
aajjfloridoo <aj.florido@iesnervion.es>
2025-09-22 10:13:15 +0200

    Update Exercise02.java

--- src/unit1/Exercise02.java
+++ src/unit1/Exercise02.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class Exercise02 {
 
 	public static void main(String[] args) {
+		// Declaramos las variables
 		int edad, edad1;
+		// Iniciamos Scanner
 		Scanner sc = new Scanner(System.in);
+		// Pedimos por teclado edad
 		System.out.println("Escriba un edad: ");
+		// La guardamos
 		edad = sc.nextInt();
+		// Calculamos la nuev edad
 		edad1 = edad + 1;
+		// La enseñamos por pantalla
 		System.out.println("\nSu edad es: " + edad1);
+		// Cerramos Scanner
 		sc.close();
 	}
 
 }
+
