2b736e8fc9a9c8bbf20148b81d2715777bf9ca51
aajjfloridoo <aj.florido@iesnervion.es>
2025-09-22 10:09:39 +0200

    Update Exercise01.java

--- src/unit1/Exercise01.java
+++ src/unit1/Exercise01.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class Exercise01 {
 
 	public static void main(String[] args) {
+		// declaramos las variables
 		int numero;
+		// Iniciamos Scanner
 		Scanner reader = new Scanner(System.in);
+		// Pedimos numero por teclado
 		System.out.println("Escriba un número: ");
+		// Hacemos que Scanner lo guarde
 		numero = reader.nextInt();
+		// Lo enseñamos por pantalla
 		System.out.println("\nSu numero es: " + numero);
+		// Cerramos Scanner
 		reader.close();
 	}
 
 }
+
