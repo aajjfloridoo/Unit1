a24ee57c318edfe8cab42695cec6aea0a37d2711
aajjfloridoo <aj.florido@iesnervion.es>
2025-09-22 10:17:48 +0200

    Update Exercise03.java

--- src/unit1/Exercise03.java
+++ src/unit1/Exercise03.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class Exercise03 {
 
 	public static void main(String[] args) {
+		// Declaramos variables
 		int añoActual, añoNacimiento, edad;
+		// Iniciamos Scanner
 		Scanner sc = new Scanner(System.in);
+		// Pedimos año actual por teclado
 		System.out.println("Escriba el año actual: ");
+		// Leemos y guardamos
 		añoActual = sc.nextInt();
+		// Pedimos año de nacimiento por teclado
 		System.out.println("Escriba su año de nacimiento: ");
+		// Leemos y guardamos
 		añoNacimiento = sc.nextInt();
+		// Calculamos la edad
 		edad = añoActual - añoNacimiento;
+		// Mostramos edad por pantalla
 		System.out.println("\nSu edad es: " + edad);
+		// Cerramos Scanner
 		sc.close();
 	}
 
 }
+
